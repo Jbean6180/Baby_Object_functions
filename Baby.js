efab57c7fd7a7823048214e278be0ee864a1fdd6
Jillian Puplampu <jgpuplampu@gmail.com>
2017-08-02 01:46:14 -0400

    Create Baby.js

---
+++ Baby.js
@@ -0,0 +1,17 @@
+var Baby = {};
+
+Baby.eat = function(){
+  console.log("nom, nom, nom");
+};
+
+Baby.poop = function(){
+  console.log("wah, wah, wah");
+};
+
+Baby.sleep = function(){
+  console.log("zzz");
+};
+
+Baby.eat();
+Baby.poop();
+Baby.sleep();
