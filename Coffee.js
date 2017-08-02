8bc3286059cbaf8c379b0d1870a666a1c6be9cc9
Jillian Puplampu <jgpuplampu@gmail.com>
2017-08-02 02:18:00 -0400

    Create Coffee.js

---
+++ Coffee.js
@@ -0,0 +1,30 @@
+console.log("coffee = happiness")
+
+var me = {};
+
+me.drink = function(){
+  console.log("*sip*");
+};
+me.sad = function(){
+  console.log("*miniature violin*");
+};
+
+var coffee = {};
+
+coffee.hot = function(){
+  console.log("ssss");
+};
+
+coffee.cold = function(){
+  console.log("*miniature violin*");
+};
+
+while (coffee == hot)
+{
+  me.drink();
+}
+
+while (coffee == cold)
+{
+  me.sad();
+}
