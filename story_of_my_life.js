3a5a103fc6056e18b0c2f2e5b67fcbc9fd0be424
Jillian Puplampu <jgpuplampu@gmail.com>
2017-08-02 02:05:44 -0400

    Create story_of_my_life.js

---
+++ story_of_my_life.js
@@ -0,0 +1,38 @@
+console.log("story of my life")
+
+var me = {};
+
+me.eat = function(){
+  console.log("yum");
+};
+me.sleep = function(){
+  console.log("zzz");
+};
+me.dream_about_matthew_gray_gubler = function(){
+  console.log("yum");
+};
+me.awake = function(){
+  console.log("woke");
+};
+me.program = function(){
+  console.log("click, click,click");
+};
+me.use_computer = function(){
+  console.log("memes");
+};
+
+
+while (me == awake)
+{
+  use computer;
+}
+
+while (me == sleep)
+{
+  dream about matthew gray gubler
+}
+
+me.awake();
+me.eat();
+me.program();
+me.sleep();
