3366a0cf32582cef024683f8c89b8ee0a80a7a3b
Jillian Puplampu <jgpuplampu@gmail.com>
2017-08-02 01:53:02 -0400

    Create Cat.js

---
+++ Cat.js
@@ -0,0 +1,27 @@
+var Cat = {};
+
+Cat.eat = function(){
+  console.log("nom, nom, nom");
+};
+
+Cat.purr = function(){
+  console.log("purrrr");
+};
+
+Cat.stare = function(){
+  console.log("");
+};
+  
+Cat.meow = function(){
+  console.log("meowwww, meoww, meow");
+};
+
+Cat.sleep = function(){
+  console.log("zzz");
+};
+
+Cat.eat();
+Cat.meow();
+Cat.stare();
+Cat.purr();
+Cat.sleep();
